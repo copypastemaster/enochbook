a41423a4785538a022edfbefbb1302085f5d3a0a
master <enoch.binas@gmail.com>
2023-07-08 14:35:13 +0800

    added state

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,11 @@ import { BiSolidUserCircle } from 'react-icons/bi';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { people } from './stories';
 import * as Dropdown from '@radix-ui/react-dropdown-menu'
-import { Avatar } from '@radix-ui/react-avatar';
 
 function Nav(props) {
- 
+  const [selectedPerson, setSelectedPerson] = React.useState('')
+  const test = (e) => setSelectedPerson(e);
+
   return (
     <div className="flex justify-between space-x-40 p-3 sticky">
       <h1 className="text-3xl md:text-xl">Enochbook</h1>
@@ -17,11 +18,12 @@ function Nav(props) {
 
         <Dropdown.DropdownMenuTrigger>
           <input
-            type="search"
+            type="text" 
             name="search"
             placeholder="Search Enochbook"
-            className="w-100 sm:w-100 md:w-40 lg:w-80 rounded-md bg-slate-100 text-sm px-2"
-            aria-label='dropdown of individuals'         
+            className="w-100 sm:w-100 md:w-40 lg:w-80 rounded-md bg-slate-100 text-sm p-2"
+            aria-label='dropdown of individuals'
+            value={selectedPerson}         
           />
         </Dropdown.DropdownMenuTrigger>
 
@@ -30,7 +32,9 @@ function Nav(props) {
             
               {people.map((person) => {
                 return(
-                  <Dropdown.Item key={person.id} className='flex gap-6 hover:bg-slate-300 rounded-md p-2'>
+                  <Dropdown.Item key={person.id} 
+                                 className='flex gap-6 hover:bg-slate-300 rounded-md p-2'
+                                 onClick={() => test(person.name)}>
                     <img src={person.img}
                         className='w-100 h-100 max-w-sm max-h-7 rounded-3xl'/>
                     <h1>{person.name}</h1>   
